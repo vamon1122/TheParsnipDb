701b1a80d09d7a1866b3d3e187033276753ae255
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2020-05-08 23:01:39 +0100

    Add a proc for selecting a single media tag #9

---
+++ Procs/[dbo].[media_tag_SELECT_WHERE_id].sql
@@ -0,0 +1,35 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Ben Barton
+-- Create date: 08/05/2020
+-- Description:	SELECT a media_tag by it's id
+-- =============================================
+-- =============================================
+-- CHANGELOG
+-- V1.0 - 08/05/2020 - Initial create 
+-- =============================================
+
+CREATE PROCEDURE [dbo].[media_tag_SELECT_WHERE_id] 
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		media_tag.[id]
+      ,[created_by_user_id]
+      ,[media_tag].[datetime_created]
+      ,[name]
+      ,[description]
+	FROM 
+		media_tag 
+		INNER JOIN [user] ON [user].id = media_tag.created_by_user_id 
+	WHERE 
+		[user].datetime_deleted IS NULL
+		AND media_tag.id = @id
+END
+GO
