d1bb1921ae603b49bf6fc5120f1939e860004201
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2020-08-22 21:40:23 +0100

    Amend video table & procs to accommodate video sequences #33

--- Tables/[dbo].[video].sql
+++ Tables/[dbo].[video].sql
@@ -1,21 +1,44 @@
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[video](
-	[media_id] [char](8) NOT NULL,
-	[x_scale] [float] NOT NULL,
-	[y_scale] [float] NOT NULL,
-	[compressed_dir] [char](1024) NULL,
-	[original_dir] [char](1024) NOT NULL,
- CONSTRAINT [PK_video_media_id] PRIMARY KEY CLUSTERED 
+
+CREATE TABLE dbo.video
 (
-	[media_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	media_id char(8) NOT NULL,
+	x_scale float NOT NULL,
+	y_scale float NOT NULL,
+	compressed_dir char(1024) NULL,
+	original_dir char(1024) NULL,
+	CONSTRAINT PK_video_media_id PRIMARY KEY CLUSTERED
+	(
+		media_id ASC
+	)
+	WITH
+	(
+		PAD_INDEX = OFF,
+		STATISTICS_NORECOMPUTE = OFF,
+		IGNORE_DUP_KEY = OFF,
+		ALLOW_ROW_LOCKS = ON,
+		ALLOW_PAGE_LOCKS = ON
+	)
+	ON [PRIMARY]
+)
+ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[video]  WITH CHECK ADD  CONSTRAINT [FK_video_media] FOREIGN KEY([media_id])
-REFERENCES [dbo].[media] ([id])
+
+ALTER TABLE dbo.video
+SET (LOCK_ESCALATION = TABLE)
 GO
-ALTER TABLE [dbo].[video] CHECK CONSTRAINT [FK_video_media]
+
+ALTER TABLE dbo.video
+ADD CONSTRAINT FK_video_media FOREIGN KEY
+(
+	media_id
+)
+REFERENCES dbo.media
+(
+	id
+)
 GO
