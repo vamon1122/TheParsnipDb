3644d4798609ada8bc931f6d7acddc5687ba81d1
= <35330025+vamon1122@users.noreply.github.com>
2021-01-19 23:44:58 +0000

    Wrong media_share view count retrieved in media share select #58

--- Procs/[dbo].[media_share_SELECT_WHERE_id].sql
+++ Procs/[dbo].[media_share_SELECT_WHERE_id].sql
@@ -1,26 +1,27 @@
-/****** Object:  StoredProcedure [dbo].[media_share_SELECT_WHERE_id]    Script Date: 29/03/2020 12:49:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
 -- Author:		Ben Barton
--- Create date: 
--- Description:	
+-- Create date: 19/01/2021
+-- Description:	19/01/2021
+-- =============================================
+-- =============================================
+-- CHANGELOG
+-- V1.0 - 19/01/2021 - Fix wrong view count retrieved
 -- =============================================
 CREATE PROCEDURE [dbo].[media_share_SELECT_WHERE_id]
 	@id char(8)
 AS
 BEGIN
 	SET NOCOUNT ON;
-
 	
-
 	SELECT 
 		[media_share].[id],
 		[created_by_user_id], 
 		[media_share].[datetime_created], 
-		(SELECT COUNT(*) media_share_view WHERE media_id = [media_id]),
+		(SELECT COUNT(*) FROM media_share_view MSV INNER JOIN media_share MS ON MSV.media_share_id = MS.id  WHERE MS.id = @id),
 		[media_id] 
 	FROM 
 		media_share 
