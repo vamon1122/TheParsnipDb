c7ca62f20c396f2f635a8f769c026f8834039f99
= <35330025+vamon1122@users.noreply.github.com>
2021-01-22 16:48:41 +0000

    log_entry_SELECT_WHERE_log_id does not order result #59

--- Procs/[dbo].[log_entry_SELECT_WHERE_log_id].sql
+++ Procs/[dbo].[log_entry_SELECT_WHERE_log_id].sql
@@ -11,6 +11,7 @@ GO
 -- CHANGELOG
 -- V1.0 - 29/03/2020 - Initial commit
 -- V1.1 - 12/06/2020 - Changed to select top 1000
+-- V1.2 - 22/01/2021 - Added ORDER BY
 -- =============================================
 CREATE PROCEDURE [dbo].[log_entry_SELECT_WHERE_log_id]
 	@log_id int
@@ -27,5 +28,7 @@ BEGIN
 		log_entry 
 	WHERE 
 		log_id = @log_id  
+	ORDER BY
+		datetime_created DESC
 END
 GO
