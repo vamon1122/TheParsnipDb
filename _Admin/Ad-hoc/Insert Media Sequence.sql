3ae8422fbd49df1fb7463b3ad8a6973332826265
= <35330025+vamon1122@users.noreply.github.com>
2021-04-29 22:30:35 +0100

    Improvements to 'Insert Media Sequence' script #77

--- _Admin/Ad-hoc/Insert Media Sequence.sql
+++ _Admin/Ad-hoc/Insert Media Sequence.sql
@@ -1,11 +1,14 @@
-DECLARE @id char(8) = 'BBBB0000'
-DECLARE @video_1_id char(8) = '84139d66'
-DECLARE @new_title char(100) = 'STITCH: Let Me Entertain You 1 & 2'
+DECLARE @id char(8) = (SELECT LEFT(NEWID(), 8))
+DECLARE @video_1_id char(8) = 'XXXXXX01'
+DECLARE @new_title char(100) = ''
+DECLARE @now datetime = (SELECT GETDATE() + '06:00:00') --BST
+--DECLARE @now datetime = (SELECT GETDATE() + '08:00:00') --GMT
 
-INSERT INTO media (id, created_by_user_id, [type], placeholder_dir, compressed_dir, original_dir, title, x_scale, y_scale)
-SELECT @id, created_by_user_id, [type], placeholder_dir, compressed_dir, original_dir, @new_title, x_scale, y_scale FROM media WHERE id = @video_1_id
+INSERT INTO media (id, created_by_user_id, [type], placeholder_dir, compressed_dir, original_dir, title, x_scale, y_scale, datetime_created, datetime_captured)
+SELECT @id, created_by_user_id, [type], placeholder_dir, compressed_dir, original_dir, @new_title, x_scale, y_scale, @now, datetime_captured FROM media WHERE id = @video_1_id
 
 INSERT INTO video (media_id) VALUES (@id)
 
 INSERT INTO video_sequence (media_id, original_media_id, position) VALUES (@id, @video_1_id, 0)
-INSERT INTO video_sequence (media_id, original_media_id, position) VALUES (@id, '071e2973', 1)
+INSERT INTO video_sequence (media_id, original_media_id, position) VALUES (@id, 'XXXXXX02', 1)
+--INSERT INTO video_sequence (media_id, original_media_id, position) VALUES (@id, 'XXXXXX03', 2)
